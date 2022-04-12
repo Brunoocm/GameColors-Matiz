5347acba79329480cce2766f439c523f05af8189
Brunoocm <brunocm64@gmail.com>
2022-04-12 17:48:45 -0300

    upa

--- Matiz/Assets/_Projeto/Scripts/Enemy/EnemyMovement.cs
+++ Matiz/Assets/_Projeto/Scripts/Enemy/EnemyMovement.cs
@@ -1,41 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour
 {
-    //public variables
 
     public float followSpeed;
     public float followRange;
     public float retreatSpeed;
     public float retreatRange;
     public LayerMask playerLayer;
+    public LayerMask enemyLayer;
 
-    //private variables
+    [Header("aa")]
+    private bool playerFound;
+    private bool otherEnemyFound;
 
+    public GameObject[] EnemyTransform;
+    public Transform closestTarget;
     GameObject player;
+    GameObject myObject;
 
-    //components
-
-    Animator anim;
-
+    Animator anim => gameObject.GetComponent<Animator>();
+    NavMeshAgent navMeshAgent => gameObject.GetComponent<NavMeshAgent>();
     private void Start()
     {
+        myObject = this.gameObject;
         player = GameObject.FindGameObjectWithTag("Player");
-
-        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        if(Physics.CheckSphere(transform.position, retreatRange, playerLayer))
+        playerFound = Physics.CheckSphere(transform.position, followRange, playerLayer);
+        otherEnemyFound = Physics.CheckSphere(transform.position, followRange, enemyLayer);
+
+        if (playerFound)
         {
-            FollowRetreat(-retreatSpeed);
+            FollowRetreat(followSpeed); //Chase player
+
+          
         }
-        else if(Physics.CheckSphere(transform.position, followRange, playerLayer))
+        if (otherEnemyFound)
         {
-            FollowRetreat(followSpeed);
+            EnemyTransform = GameObject.FindGameObjectsWithTag("Enemy");
+            CheckIsActive();
+            MoveAway(followSpeed);
+        }
+        else if (Physics.CheckSphere(transform.position, retreatRange, playerLayer))
+        {
+            //FollowRetreat(-retreatSpeed);
+        }
+        else if (Physics.CheckSphere(transform.position, followRange, enemyLayer))
+        {
         }
         else
         {
@@ -48,12 +65,52 @@ public class EnemyMovement : MonoBehaviour
 
     void FollowRetreat(float speed)
     {
-        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
-        
+        Vector3 target = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+        navMeshAgent.SetDestination(target);
+
         if(anim != null)
         {
             anim.SetBool("walk", true);
         }
+    }
+
+    void MoveAway(float speed)
+    {
+        Vector3 target = new Vector3(closestTarget.transform.position.x, transform.position.y, closestTarget.transform.position.z);
+        navMeshAgent.SetDestination(-target * 10);
+    }
+
+    void CheckIsActive()
+    {
+
+        float closestDistanceSqr = Mathf.Infinity;
+        Vector3 currentPosition = transform.position;
+
+        foreach (GameObject pointsTarget in EnemyTransform)
+        {
+            Vector3 directionToTarget = pointsTarget.transform.position - currentPosition;
+            float dSqrToTarget = directionToTarget.sqrMagnitude;
+            if (dSqrToTarget < closestDistanceSqr)
+            {
+                //for (int i = 0; i < EnemyTransform.Length; i++)
+                //{
+                //    pointsTarget++;
+                //}
+
+                if (pointsTarget == myObject)
+                    continue;
+                closestDistanceSqr = dSqrToTarget;
+                closestTarget = pointsTarget.transform;
+
+            }
+            else
+            {
+
+            }
+
+
+        }
+
     }
 
     void OnDrawGizmosSelected()
