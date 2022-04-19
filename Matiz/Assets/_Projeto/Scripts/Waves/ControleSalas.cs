5ccd068db1005e45ab7b3572be1bf0089f48584b
Brunoocm <brunocm64@gmail.com>
2022-04-19 01:16:51 -0300

    arena

--- Matiz/Assets/_Projeto/Scripts/Waves/ControleSalas.cs
+++ Matiz/Assets/_Projeto/Scripts/Waves/ControleSalas.cs
@@ -15,6 +15,7 @@ public class ControleSalas : MonoBehaviour
     public GameObject ranged;
     public GameObject melee;
     public GameObject explosive;
+    private bool oneTime;
 
     [Header("PROGRESSÃO")]
     public Level[] levels;
@@ -23,16 +24,38 @@ public class ControleSalas : MonoBehaviour
     public Level currentLevel;
     private LayoutWave currentLayout;
     public List<GameObject> inimigosDisponiveis;
+    public GameObject[] currentEnemies;
     private int currentLevelNumber = 0;
 
 
     private void Awake()
     {
-        StartCoroutine(StartGameplay());
+    }
+
+    private void Update()
+    {
+        FindEnemies();
+        print(currentEnemies.Length);
+        if (currentEnemies.Length <= 0 && oneTime)
+        {
+            oneTime = false;
+
+            NextLevel();
+        }
+        else
+        {
+            oneTime = true;
+
+        }
+        //if (Input.GetKeyDown(KeyCode.N))
+        //{
+        //    NextLevel();
+        //}
     }
 
     private void SpawnEnemies()
     {
+
         inimigosDisponiveis.Clear();
 
         if (currentLevel.inimigos.melee) inimigosDisponiveis.Add(melee);
@@ -43,6 +66,13 @@ public class ControleSalas : MonoBehaviour
         {
             Instantiate(inimigosDisponiveis[Random.Range(0, inimigosDisponiveis.Count)], spawnpoint.position, spawnpoint.rotation);
         }
+        oneTime = true;
+
+    }
+
+    public void FindEnemies()
+    {
+        currentEnemies = GameObject.FindGameObjectsWithTag("Enemy");
     }
 
     IEnumerator StartGameplay()
@@ -50,12 +80,20 @@ public class ControleSalas : MonoBehaviour
         currentLevelNumber++;
         currentLevel = levels[currentLevelNumber - 1];
 
-        //if(currentLayout != null)currentLayout.visual.SetActive(false);
 
         currentLayout = layouts[currentLevelNumber - 1];
-        //currentLayout.visual.SetActive(true);
 
         yield return new WaitForSeconds(0.5f);
+
+        SpawnEnemies();
+    }
+    void NextLevel()
+    {
+        currentLevelNumber++;
+        currentLevel = levels[currentLevelNumber - 1];
+
+        currentLayout = layouts[currentLevelNumber - 1];
+
 
         SpawnEnemies();
     }
