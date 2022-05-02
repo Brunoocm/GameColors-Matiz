0495ec5e66850e8fe619e3b015e6319d813402a4
Brunoocm <brunocm64@gmail.com>
2022-05-02 05:27:23 -0300

    b

---
+++ Matiz/Assets/_Projeto/Scripts/Player/CharacterAbilities.cs
@@ -0,0 +1,225 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAbilities : MonoBehaviour
+{
+    public bool cinzaTrue;
+    public bool vermelhoTrue;
+    public bool azulTrue;
+    public bool verdeTrue;
+
+    public CinzaAbility cinzaAbility;
+    public VermelhoAbility vermelhoAbility;
+    public AzulAbility azulAbility;
+    public VerdeAbility verdeAbility;
+
+    CharacterStats characterStats => GetComponent<CharacterStats>();
+
+    private float time;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (cinzaTrue)
+        {
+            cinzaAbility.Especial();
+            cinzaAbility.Dash();
+        }
+        else if (vermelhoTrue)
+        {
+            vermelhoAbility.Passiva();
+            vermelhoAbility.Especial();
+            vermelhoAbility.Dash();
+
+            print("vermelho true");
+
+            //if(time <= vermelhoAbility.specialCooldown)
+            //{
+            //    time += Time.deltaTime;
+            //}
+            //else
+            //{
+            //    if (Input.GetKeyDown(KeyCode.Mouse1))
+            //    {
+            //        GameObject red = Instantiate(vermelhoAbility.specialVermelho, transform.position, Quaternion.identity);
+            //        red.transform.parent = transform;
+            //        time = 0;
+            //    }
+            //}
+        }
+        else if (azulTrue)
+        {
+            azulAbility.Passiva();
+            azulAbility.Especial();
+            azulAbility.Dash();
+        }
+        else if (verdeTrue)
+        {
+            verdeAbility.Passiva();
+            verdeAbility.Especial();
+            verdeAbility.Dash();
+        }
+    }
+
+    public void spawnEspecialCinza()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, cinzaAbility.groundLayer)) // 6 = layermask ground
+            {
+                GameObject obj = Instantiate(cinzaAbility.specialCinzaObj, new Vector3(hit.point.x, hit.point.y + 1, hit.point.z), Quaternion.identity);
+                obj.GetComponent<SpecialCinza>().forceKnockback = cinzaAbility.forceKnockback;
+                obj.GetComponent<SpecialCinza>().timeKnockback = cinzaAbility.timeKnockback;
+            }
+        }
+    }
+
+    public void SpecialRed()
+    {
+        print("special redding");
+
+        if (time <= vermelhoAbility.specialCooldown)
+        {
+            print("1");
+
+            time += Time.deltaTime;
+        }
+        else
+        {
+            print("2");
+
+            if (Input.GetKeyDown(KeyCode.Mouse1))
+            {
+                print("clicking mouse 1");
+
+                GameObject red = Instantiate(vermelhoAbility.specialVermelho, transform.position, Quaternion.identity);
+                red.transform.parent = transform;
+                time = 0;
+            }
+        }
+    }
+
+
+
+    [System.Serializable]
+    public class CinzaAbility
+    {
+        public CharacterAbilities characterAbilities;
+
+        [Header("Passiva")]
+        public int numStacks;
+
+        [Header("Especial")]
+        public float timeKnockback;
+        public float forceKnockback;
+        public GameObject specialCinzaObj;
+        public GameObject dashFX;
+        public LayerMask groundLayer;
+
+
+        [HideInInspector]
+        public int currentStacks;
+
+        public void PassivaTeste()
+        {
+            //HABILIDADE CINZA Script: EnemyHealth
+        }
+
+        public void Especial()
+        {
+            characterAbilities.spawnEspecialCinza();
+        }
+
+        public void Dash()
+        {
+
+        }
+
+    }
+    [System.Serializable]
+    public class VermelhoAbility
+    {
+        public int dashDamage, passiveDamage;
+        public float dashMinForce, dashMaxForce, specialCooldown, passiveSpeed;
+        public GameObject specialVermelho;
+        public GameObject dashFX;
+
+        public CharacterStats characterStats;
+        public CharacterMovement characterMove;
+        public CharacterAbilities characterAbilities;
+
+        public void Passiva()
+        {
+            //quanto menor for a ratio de vida, maior a velocidade e dano (puxar nos scripts de movimentação e stats)
+
+            if(characterStats.health == 1)
+            {
+                characterStats.damage = passiveDamage;
+                characterMove.speed = passiveSpeed;
+            }
+            else
+            {
+                characterStats.damage = characterStats.m_damage;
+                characterMove.speed = characterMove.m_speed;
+            }
+        }
+
+        public void Especial()
+        {
+            characterAbilities.SpecialRed();
+        }
+
+        public void Dash()
+        {
+            //
+        }
+
+    }
+    [System.Serializable]
+    public class AzulAbility
+    {
+
+        public void Passiva()
+        {
+
+        }
+
+        public void Especial()
+        {
+
+        }
+
+        public void Dash()
+        {
+
+        }
+
+    }
+    [System.Serializable]
+    public class VerdeAbility
+    {
+
+        public void Passiva()
+        {
+
+        }
+
+        public void Especial()
+        {
+
+        }
+
+        public void Dash()
+        {
+
+        }
+    }
+}
---
+++ Matiz/Assets/_Projeto/Scripts/Player/CharacterMovement.cs
@@ -0,0 +1,200 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+
+    public float timeBTWDash;
+    public float timeDash;
+    public float forceDash;
+
+    public LayerMask groundLayer;
+
+    [HideInInspector]
+    public bool canMove;
+    [HideInInspector]
+    public bool canDash;
+
+    public float speed = 2f;
+    [HideInInspector] public float m_speed;
+
+    public float trailCooldown;
+    public GameObject trailFX;
+    public GameObject dashFX;
+    public Transform feet;
+
+    private bool dashing;
+    private float dashMeter;
+    private float xMove, yMove;
+    private float trailTime;
+    Vector3 lastDir;
+    Vector3 moveDir;
+
+    private Rigidbody rb => gameObject.GetComponent<Rigidbody>();
+    private SpriteRenderer spriteRenderer => gameObject.GetComponentInChildren<SpriteRenderer>();
+    private CharacterController characterController => gameObject.GetComponent<CharacterController>();
+    private CharacterAttack characterAttack => gameObject.GetComponent<CharacterAttack>();
+    private CharacterAbilities characterAbilities => gameObject.GetComponent<CharacterAbilities>();
+    private CharacterStats characterStats => gameObject.GetComponent<CharacterStats>();
+    [HideInInspector] public Animator anim => gameObject.GetComponentInChildren<Animator>();
+
+    private Vector3 _movement;
+
+    private Boat boat;
+
+    private void Start()
+    {
+        canMove = true;
+        canDash = true;
+
+        boat = FindObjectOfType<Boat>();
+
+        m_speed = speed;
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !characterAbilities.vermelhoTrue)
+        {
+            if (canMove && canDash)
+            {
+                StartCoroutine(Dash(timeDash));
+            }
+        }
+
+
+        //print(dashMeter);
+
+        if (characterAbilities.vermelhoTrue)
+        {
+            if (Input.GetKey(KeyCode.Space) && dashMeter < characterAbilities.vermelhoAbility.dashMaxForce)
+            {
+                canMove = false;
+
+                dashMeter += Time.deltaTime / 5;
+            }
+
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                if(dashMeter >= characterAbilities.vermelhoAbility.dashMinForce)
+                {
+                    StartCoroutine(Dash(dashMeter));
+                }
+                else
+                {
+                    canMove = true;
+                }
+            }
+        }
+    }
+
+    void PlayerMove(float xMove, float yMove)
+    {
+        if (moveDir.magnitude > 0.1f)
+        {
+            characterController.Move(moveDir * speed * Time.deltaTime);
+
+            trailTime -= Time.deltaTime;
+
+            if (trailTime <= 0)
+            {
+                Instantiate(trailFX, feet.position, Quaternion.identity);
+                trailTime = trailCooldown;
+            }
+        }
+
+        anim.SetFloat("MoveX", xMove);
+        anim.SetFloat("MoveZ", yMove);
+        anim.SetFloat("Speed", moveDir.sqrMagnitude);
+    }
+
+    public IEnumerator Dash(float dashTime)
+    {
+        dashMeter = 0;
+
+        if (characterAbilities.cinzaTrue || characterAbilities.vermelhoTrue)
+        {
+            characterStats.timeInvencible = characterStats.m_timeInvencible;
+            characterStats.canDamage = false;
+        }
+
+        canMove = false;
+        canDash = false;
+
+        float xMove = Input.GetAxisRaw("Horizontal");
+        float yMove = Input.GetAxisRaw("Vertical");
+        Vector3 moveDir = new Vector3(xMove, 0f, yMove).normalized;
+
+        anim.SetTrigger("DashTrigger");
+        anim.SetFloat("Vertical", yMove);
+        anim.SetFloat("Horizontal", xMove);
+
+        float startTime = Time.time;
+        while (Time.time < startTime + dashTime)
+        {
+            if (characterAbilities.vermelhoTrue)
+            {
+                Instantiate(characterAbilities.vermelhoAbility.dashFX, feet.position, Quaternion.identity);
+            }
+
+            if (characterAbilities.cinzaTrue)
+            {
+                Instantiate(characterAbilities.cinzaAbility.dashFX, feet.position, Quaternion.identity);
+            }
+
+            dashing = true;
+            //transform.Translate(moveDir * forceDash * Time.deltaTime);
+            transform.Translate(lastDir * forceDash * Time.deltaTime);
+            yield return null;
+        }
+
+        canMove = true;
+
+        yield return new WaitForSeconds(timeBTWDash);
+        canDash = true;
+        dashing = false;
+    }
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        moveDir = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (canMove) PlayerMove(horizontal, vertical);
+
+        if (moveDir != Vector3.zero)
+        {
+            lastDir = moveDir;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Harbor"))
+        {
+            boat.inHarbor = true;
+        }
+
+        if (other.gameObject.CompareTag("Enemy") && other.GetComponent<EnemyHealth>() != null && dashing)
+        {
+            if (characterAbilities.cinzaTrue)
+            {
+                other.GetComponent<EnemyHealth>().DamageVoid(0);
+            }
+            else if (characterAbilities.vermelhoTrue)
+            {
+                other.GetComponent<EnemyHealth>().DamageVoid(characterAbilities.vermelhoAbility.dashDamage);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Harbor"))
+        {
+            boat.inHarbor = false;
+        }
+    }
+}
