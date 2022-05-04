bdadac13aecd7aa53c75f239cc6d7d1a9ddcf1b6
Brunoocm <brunocm64@gmail.com>
2022-05-04 05:29:04 -0300

    ARRUMEI O MALDITO BUG DO BARCO

---
+++ Matiz/Assets/TutorialDelmar.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+namespace OniricoStudios
+{
+
+
+    public class TutorialDelmar : MonoBehaviour
+    {
+        private static TutorialDelmar playerInstance;
+
+        public Transform CinzaPos;
+        public Transform VermelhaPos;
+
+        void Awake()
+        {
+            DontDestroyOnLoad(this);
+
+            if (playerInstance == null)
+            {
+                playerInstance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))//cinza
+            {
+                StartCoroutine(loadsceneName("MainLand", CinzaPos));
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha2))//vermalha
+            {
+                StartCoroutine(loadsceneName("MainLand", VermelhaPos));
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha3))//arena
+            {
+                SceneManager.LoadScene("PrimeiraArena");
+            }
+        }
+
+        IEnumerator loadsceneName(string name, Transform pos)
+
+        {
+            SceneManager.LoadScene(name);
+            while (SceneManager.GetActiveScene().name != name)
+            {
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(1f);
+            CharacterStats.playerObj.gameObject.transform.position = pos.position;
+        }
+    }
+}
