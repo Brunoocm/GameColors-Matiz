06771350c58a4d2a25c81158e832d5963c3c773e
Brunoocm <brunocm64@gmail.com>
2022-05-29 02:53:20 -0300

    compass certo, só falta arrumar o tamanho maximo

--- Matiz/Assets/_Projeto/Scripts/Player/PrismaScripts/PrismCompass.cs
+++ Matiz/Assets/_Projeto/Scripts/Player/PrismaScripts/PrismCompass.cs
@@ -29,18 +29,23 @@ namespace OniricoStudios
 
                 float arrowSize = Vector3.Distance(prism.transform.position, transform.position) / 10;
 
-                if (arrowSize > 0.5f && arrowSize < 1.5f)
-                {
-                    compassArrow.transform.localScale = new Vector3(arrowSize, arrowSize, arrowSize);
-                }
-                else if(arrowSize <= 0.5f)
-                {
-                    compassArrow.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
-                }
-                else if(arrowSize >= 1.5f)
-                {
-                    compassArrow.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
-                }
+                //compassArrow.transform.localScale = new Vector3(arrowSize, arrowSize, arrowSize);
+
+                compassArrow.transform.localScale = (new Vector3(0.4f, 0.4f, 0.4f) / arrowSize) * 2.3f;
+
+                //if (arrowSize > 0.5f && arrowSize < 1.5f)
+                //{
+
+                //}
+                //if (arrowSize >= 1.5f)
+                //{
+                //    compassArrow.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+                //}
+                //else if (arrowSize <= 0.5f)
+                //{
+                //    compassArrow.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+                //}
+
             }
             else
             {
